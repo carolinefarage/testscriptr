f30c428a3996962588c886d47955b433fb2bd2df
carolinefarage <caroline.farage@gmail.com>
2016-07-05 18:52:00 +0300

    test

--- scripts/test2.js
+++
@@ -1,7 +0,0 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"return \"hello\";"},"scriptrdata":[]}}*#*#*/
-var content= 'return \"hello\";';  response.write(content);response.close();			
